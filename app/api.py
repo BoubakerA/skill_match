55db72f615e922005e8daa5f685447e9f8d35734
boubaker <boubaker.asaadi@ensae.fr>
2026-03-15 15:04:34 +0000

    Fix the api serving

--- app/api.py
+++ app/api.py
@@ -13,6 +13,7 @@ logging.basicConfig(
 
 app = FastAPI(
     title="Matching des compétences entre un candidat et une offre d'emploi",
+    root_path="/api",
     description=
     "Application de matching des compétences entre un candidat et une offre d'emploi." +\
         "<br><br><img src=\"https://www.mykonekti.fr/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpuzzle.9ccecc64.png&w=3840&q=75\" width=\"200\">"
